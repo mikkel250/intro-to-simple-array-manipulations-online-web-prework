232f7feec023306eb7be544c111cd6e13c5a0a0e
Mikkel Ridley <mikkel250@gmail.com>
2018-09-07 05:41:04 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(arr, str)
+  arr << str
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr, num)
+  arr.pop(num)
+end
+
